4fde88aab6b7adbf845e7df7d23f699aba9745d5
Sara <sara.riosa@udea.edu.co>
2022-08-12 16:37:59 -0500

    placeholder

--- resources/views/controles/create.blade.php
+++ resources/views/controles/create.blade.php
@@ -3,71 +3,73 @@
 @section('title' , 'Ingresar nuevo control')
 
 @section('content')
+<div class='container'>
     <form action="{{route('controles.index')}}" method="post">
         @csrf
         <div class="mb-3">
             <label for="" class="form-label">Nivel 1 TP: </label>
-            <input type="text" id="tp1" name="tp1" class="form-control" tabindex="2">
+            <input type="text" id="tp1" name="tp1" class="form-control" tabindex="2" placeholder="Ingrese nivel 1 de TP">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 1 PTT: </label>
-            <input type="text" id="ptt1" name="ptt1" class="form-control" tabindex="3">
+            <input type="text" id="ptt1" name="ptt1" class="form-control" tabindex="3" placeholder="Ingrese nivel 1 de PTT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 1 AT: </label>
-            <input type="text" id="at1" name="at1" class="form-control" tabindex="4">
+            <input type="text" id="at1" name="at1" class="form-control" tabindex="4" placeholder="Ingrese nivel 1 de AT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label"> Nivel 1 TT: </label>
-            <input type="text" id="tt1" name="tt1" class="form-control" tabindex="5">
+            <input type="text" id="tt1" name="tt1" class="form-control" tabindex="5" placeholder="Ingrese nivel 1 de TT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 1 FB: </label>
-            <input type="text" id="fb1" name="fb1" class="form-control" tabindex="6">
+            <input type="text" id="fb1" name="fb1" class="form-control" tabindex="6" placeholder="Ingrese nivel 1 de FB">
         </div>
 
         <div class="mb-3">
             <label for="" class="form-label">Nivel 2 TP: </label>
-            <input type="text" id="tp2" name="tp2" class="form-control" tabindex="7">
+            <input type="text" id="tp2" name="tp2" class="form-control" tabindex="7" placeholder="Ingrese nivel 2 de TP">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 2 PTT: </label>
-            <input type="text" id="ptt2" name="ptt2" class="form-control" tabindex="8">
+            <input type="text" id="ptt2" name="ptt2" class="form-control" tabindex="8" placeholder="Ingrese nivel 2 de PTT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 2 AT: </label>
-            <input type="text" id="at2" name="at2" class="form-control" tabindex="9">
+            <input type="text" id="at2" name="at2" class="form-control" tabindex="9" placeholder="Ingrese nivel 2 de AT"> 
         </div>
         <div class="mb-3">
             <label for="" class="form-label"> Nivel 2 TT: </label>
-            <input type="text" id="tt2" name="tt2" class="form-control" tabindex="10">
+            <input type="text" id="tt2" name="tt2" class="form-control" tabindex="10" placeholder="Ingrese nivel 2 de TT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 2 FB: </label>
-            <input type="text" id="fb2" name="fb2" class="form-control" tabindex="11">
+            <input type="text" id="fb2" name="fb2" class="form-control" tabindex="11" placeholder="Ingrese nivel 2 de FB">
         </div>
 
         <div class="mb-3">
             <label for="" class="form-label">Nivel 3 TP: </label>
-            <input type="text" id="tp3" name="tp3" class="form-control" tabindex="12">
+            <input type="text" id="tp3" name="tp3" class="form-control" tabindex="12" placeholder="Ingrese nivel 3 de TP">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 3 PTT: </label>
-            <input type="text" id="ptt3" name="ptt3" class="form-control" tabindex="13">
+            <input type="text" id="ptt3" name="ptt3" class="form-control" tabindex="13" placeholder="Ingrese nivel 3 de PTT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 3 AT: </label>
-            <input type="text" id="at3" name="at3" class="form-control" tabindex="14">
+            <input type="text" id="at3" name="at3" class="form-control" tabindex="14" placeholder="Ingrese nivel 3 de AT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label"> Nivel 3 TT: </label>
-            <input type="text" id="tt3" name="tt3" class="form-control" tabindex="15">
+            <input type="text" id="tt3" name="tt3" class="form-control" tabindex="15" placeholder="Ingrese nivel 3 de TT">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nivel 3 FB: </label>
-            <input type="text" id="fb3" name="fb3" class="form-control" tabindex="16">
+            <input type="text" id="fb3" name="fb3" class="form-control" tabindex="16" placeholder="Ingrese nivel 3 de FB">
         </div>
         <a href="/controles" class="btn bnt-secondary" tabindex="5">Cancelar</a>
         <button type="submit" class= "btn bnt-primary" tabindex="6">Guardar</button>
     </form>
+</div>
 @endsection
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,54 +3,56 @@
 @section('title' , 'Ingresar nuevo ususario')
 
 @section('content')
+<div class='container'>
     <form action="/users" method="post">
         @csrf
         <div class="mb-3">
             <label for="" class="form-label">Documento:</label>
-            <input type="text" id="documento" name="documento" class="form-control" tabindex="2">
+            <input type="text" id="documento" name="documento" class="form-control" tabindex="2" placeholder="Ingrese el documento de identidad">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Nombre: </label>
-            <input type="text" id="nombre" name="nombre" class="form-control" tabindex="3">
+            <input type="text" id="nombre" name="nombre" class="form-control" tabindex="3" placeholder="Ingrese el nombre">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Apellidos: </label>
-            <input type="text" id="apellido" name="apellido" class="form-control" tabindex="4">
+            <input type="text" id="apellido" name="apellido" class="form-control" tabindex="4" placeholder="Ingrese los apellidos">
         </div>
         <div class="mb-3">
-            <label for="" class="form-label">eps: </label>
-            <input type="text" id="eps" name="eps" class="form-control" tabindex="5">
+            <label for="" class="form-label">Eps: </label>
+            <input type="text" id="eps" name="eps" class="form-control" tabindex="5" placeholder="Ingrese la eps">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Género: </label>
-            <input type="text" id="genero" name="genero" class="form-control" tabindex="6">
+            <input type="text" id="genero" name="genero" class="form-control" tabindex="6" placeholder="Ingrese género">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">Edad: </label>
-            <input type="text" id="edad" name="edad" class="form-control" tabindex="7">
+            <input type="text" id="edad" name="edad" class="form-control" tabindex="7" placeholder="Ingrese la edad">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">TP: </label>
-            <input type="text" id="tp" name="tp" class="form-control" tabindex="8">
+            <input type="text" id="tp" name="tp" class="form-control" tabindex="8" placeholder="Ingrese Tiempo de protombina">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">PTT: </label>
-            <input type="text" id="ptt" name="ptt" class="form-control" tabindex="9">
+            <input type="text" id="ptt" name="ptt" class="form-control" tabindex="9" placeholder=" Ingrese Tiempo de Tromboplastina Parcial">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">AT: </label>
-            <input type="text" id="at" name="at" class="form-control" tabindex="10">
+            <input type="text" id="at" name="at" class="form-control" tabindex="10" placeholder="Ingrese Tiempo de Antitrombina">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">TT: </label>
-            <input type="text" id="tt" name="tt" class="form-control" tabindex="11">
+            <input type="text" id="tt" name="tt" class="form-control" tabindex="11" placeholder="Ingrese Tiempo de Trombina">
         </div>
         <div class="mb-3">
             <label for="" class="form-label">FB: </label>
-            <input type="text" id="fb" name="fb" class="form-control" tabindex="12">
+            <input type="text" id="fb" name="fb" class="form-control" tabindex="12" placeholder="Fibrinógeno">
         </div>
         <a href="/users" class="btn bnt-secondary" tabindex="5">Cancelar</a>
         <button type="submit" class= "btn bnt-primary" tabindex="6">Guardar</button>
     </form>
+</div>
 @endsection
 
